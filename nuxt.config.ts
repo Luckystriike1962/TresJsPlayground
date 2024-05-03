321ebbbbd56d96ff58741c5d8d375a09fc70804c
luckystriike1962 <glennyengelen@live.com>
2024-05-03 22:39:54 +0200

    basic sphere setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['vuetify/lib/styles/main.sass'],
+  components: true,
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: ['@tresjs/nuxt'],
 })
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+
+<template>
+    
+    <TresCanvas window-size :clear-color="'gray'">
+        <TresMesh>
+            <TresSphereGeometry ref="sphereRef"></TresSphereGeometry>
+            <TresMeshStandardMaterial :color="'red'"/>
+        </TresMesh>
+        <TresDirectionalLight :position="[1,1,1]"/>
+    </TresCanvas>
+</template>
+
+<script lang="ts" setup>
+import type { ShallowRef } from "vue";
+
+const sphereRef: ShallowRef<TresInstance | null> = shallowRef(null)
+
+
+</script>
+
+<style>
+
+</style>
