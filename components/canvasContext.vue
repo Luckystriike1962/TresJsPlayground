e78807b242e32628809f967a2b96b869652c5dc6
luckystriike1962 <glennyengelen@live.com>
2024-05-03 23:00:05 +0200

    background not loading

---
+++ components/canvasContext.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+</script>
+<script lang="ts" setup>
+import { useTresContext } from '@tresjs/core'
+import * as THREE from "three"
+
+let textures_skybox = [
+  "/background.png",
+  "/background.png",
+  "/background.png",
+  "/background.png",
+  "/background.png",
+  "/background.png"
+];
+
+const {scene} = useTresContext()
+scene.value.background = new THREE.CubeTextureLoader().load(
+    textures_skybox
+  );
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,15 @@
 <template>
     
     <TresCanvas window-size :clear-color="'gray'">
+        <CanvasContext/>
+
+    <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
         <TresMesh>
             <TresSphereGeometry ref="sphereRef"></TresSphereGeometry>
             <TresMeshStandardMaterial :color="'red'"/>
         </TresMesh>
         <TresDirectionalLight :position="[1,1,1]"/>
+        <TresAmbientLight :intensity="10"/>
     </TresCanvas>
 </template>
 
