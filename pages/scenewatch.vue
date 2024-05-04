17121dda334c34079b478aca1c51df44b9e7d6a6
luckystriike1962 <glennyengelen@live.com>
2024-05-04 14:16:38 +0200

    add scene watcher

---
+++ pages/scenewatch.vue
@@ -0,0 +1,42 @@
+<template>
+    <TresCanvas window-size :clear-color="'gray'" ref="canvasRef">
+        <OrbitControls/>
+        <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
+        <TresMesh>
+            <TresSphereGeometry ref="sphereRef"></TresSphereGeometry>
+            <TresMeshStandardMaterial :color="'red'" />
+        </TresMesh>
+        <TresDirectionalLight :position="[1, 1, 1]" />
+        <TresAmbientLight :intensity="10" />
+    </TresCanvas>
+</template>
+
+<script lang="ts" setup>
+// https://cientos.tresjs.org/guide/staging/environment.html#texture-reference:~:text=%3C-,Environment,-%3Afiles%3D
+import type { ShallowRef } from "vue";
+import * as THREE from 'three'
+import { OrbitControls } from "@tresjs/cientos";
+
+const canvasRef = ref();
+const sphereRef: ShallowRef<TresInstance | null> = shallowRef(null)
+let textures_skybox = [
+    "/background.png",
+    "/background.png",
+    "/background.png",
+    "/background.png",
+    "/background.png",
+    "/background.png"
+];
+
+watch(canvasRef, (value) => {
+    if (!value.context?.scene) return;
+
+    value.context.scene.value.background = new THREE.CubeTextureLoader().load(
+        textures_skybox
+    );
+});
+
+
+</script>
+
+<style></style>
